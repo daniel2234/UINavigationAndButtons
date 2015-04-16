1e01532e70efbe9ed314c15e9079cca8248ed297
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-16 14:26:00 -0400

    intial commit

---
+++ UINavigationAndButtons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UINavigationAndButtons
+//
+//  Created by Daniel Kwiatkowski on 2015-04-16.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
+
---
+++ UINavigationAndButtons/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UINavigationAndButtons
+//
+//  Created by Daniel Kwiatkowski on 2015-04-16.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
