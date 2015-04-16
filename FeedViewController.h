9d9a9b5c6480ab607500412f2662662d589224a3
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-16 15:40:41 -0400

    added tabs

---
+++ FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  UINavigationAndButtons
+//
+//  Created by Daniel Kwiatkowski on 2015-04-16.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FeedViewController : ViewController
+
+@end
---
+++ ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  UINavigationAndButtons
+//
+//  Created by Daniel Kwiatkowski on 2015-04-16.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ProfileViewController : ViewController
+
+@end
