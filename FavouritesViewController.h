0633c2bdb5dd8e6d285f37d5607ac51f6eb6d822
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-16 15:18:17 -0400

    added show profile method

---
+++ FavouritesViewController.h
@@ -0,0 +1,15 @@
+//
+//  FavouritesViewController.h
+//  UINavigationAndButtons
+//
+//  Created by Daniel Kwiatkowski on 2015-04-16.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FavouritesViewController : ViewController
+
+-(void) showProfile:(UIButton*) sender;
+
+@end
